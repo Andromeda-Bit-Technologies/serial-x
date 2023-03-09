c444ad35fa2553dbc0adaf0f559b8c4df1c7065d
jhaboic <josiphaboic@gmail.com>
2023-03-09 11:52:11 +0100

    update

--- src/js/components/port-scanner.js
+++ src/js/components/port-scanner.js
@@ -1,21 +1,50 @@
 const { invoke } = window.__TAURI__.tauri;
 
 
+export function scanner() {
+	let intervalID;
+
+	return function() {
+		clearInterval(intervalID);
+		intervalID = setInterval(() => {
+			invoke('scan_ports').then((data) => {
+				let event = new CustomEvent('port-scan-done', { bubbles: true, detail: data });
+				this.dispatchEvent(event);
+			});
+		}, Number(this.getAttribute('scan-interval')) || 3000);
+	}
+}
 
 export class PortScanner extends HTMLElement {
 	constructor() {
 		super();
 		this.style.display = 'none';
 		this.id = 'port-scanner';
+		this.intervalID = undefined;
+		this.startScan.bind(this);
 	}
 
-	connectedCallback() {
-		setInterval(() => {
+	startScan() {
+		this.intervalID = setInterval(() => {
 			invoke('scan_ports').then((data) => {
-				let event = new CustomEvent('port-scan-done', {bubbles: true, detail: data});
+				let event = new CustomEvent('port-scan-done', { bubbles: true, detail: data });
 				this.dispatchEvent(event);
 			});
 		}, Number(this.getAttribute('scan-interval')) || 3000);
+	}
+
+	connectedCallback() {
+		this.startScan();
+	}
+
+	static get observedAttributes() {
+		return ['scan-interval',]
+	}
+
+	attributeChangedCallback(name, oldValue, newValue) {
+		console.log(this.intervalID);
+		console.log(clearInterval(this.intervalID));
+		this.startScan();
 	}
 }
 
